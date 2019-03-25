d347fff0de6c0c96cd6302cf6dfe924e8201ab57
wangxiaolong11 <wangxiaolong11@meituan.com>
2019-03-25 16:34:32 +0800

    ADD: 上报功能

---
+++ scripts/setPackageJSON.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const fs = require('fs');
+
+const pkgPath = path.join(__dirname, '../package.json');
+const pkgJSON = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
+const options = process.argv;
+
+if (options[2] && options[2] === '--add') {
+  pkgJSON.scripts.postinstall = 'node ./dist/roo-report.js';
+} else {
+  delete pkgJSON.scripts.postinstall;
+}
+
+fs.writeFileSync(pkgPath, JSON.stringify(pkgJSON, null, 4), null, 'utf-8');
