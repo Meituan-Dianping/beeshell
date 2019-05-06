102f20060d5ffeb0000423546a389dd2c851704d
wxlworkhard <wangxiaolong11@meituan.com>
2019-05-06 13:04:40 +0800

    MOD: 主题变量调整

--- examples/customTheme.ts
+++ examples/customTheme.ts
@@ -3,8 +3,8 @@ import { useTheme } from '../src/common/styles/variables'
 // 获取自定义主题的变量
 // 以下是示例
 const customTheme = {
-  // mtdBrandPrimary: '#ffd800',
-  // mtdBrandPrimaryDark: '#FFA000',
+  mtdBrandPrimary: '#ffd800',
+  mtdBrandPrimaryDark: '#FFA000',
   mtdBrandSuccess: '#61cb28',
   mtdBrandWarning: '#ff8400',
   mtdBrandDanger: '#f23244',
@@ -15,8 +15,8 @@ const customTheme = {
   // mtdRadiusXS: 30,
   // buttonBorderRadius: 30,
 
-  mtdBrandPrimary: '#f23244',
-  mtdBrandPrimaryDark: '#f23244',
+//   mtdBrandPrimary: '#f23244',
+//   mtdBrandPrimaryDark: '#f23244',
 
 }
 
