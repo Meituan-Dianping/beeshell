8d5115016e7c559445248335ff3fbc982dd3ff57
wangxiaolong11 <wangxiaolong11@meituan.com>
2019-05-17 23:06:00 +0800

    MOD: 推广文章第二版完成

--- e2e/app.test.js
+++ e2e/app.test.js
@@ -15,4 +15,25 @@ describe('app test', () => {
     await element(by.id('Icon')).tap()
     await element(by.id('back').withAncestor(by.id('navigationBarIcon'))).tap()
   })
+
+  it('NavigationBar screen', async () => {
+    await element(by.id('NavigationBar')).tap()
+    await element(by.id('back').withAncestor(by.id('nav1'))).tap()
+    await expect(element(by.id('text'))).toBeVisible()
+    await element(by.id('forward').withAncestor(by.id('nav1'))).tap()
+    await element(by.id('back').withAncestor(by.id('navigationBarNavigationBar'))).tap()
+  })
+
+  it('Dropdown screen', async () => {
+    await element(by.id('Dropdown')).tap()
+    await expect(element(by.id('a1'))).toBeVisible()
+    await element(by.id('a1')).tap()
+    await expect(element(by.id('o1'))).toBeVisible()
+    await element(by.id('o1')).tap()
+    await element(by.id('a1')).tap()
+    await expect(element(by.id('backdrop'))).toBeVisible()
+    await element(by.id('backdrop')).tap()
+    await expect(element(by.id('back').withAncestor(by.id('navigationBarDropdown')))).toBeVisible()
+    await element(by.id('back').withAncestor(by.id('navigationBarDropdown'))).tap()
+  })
 })
